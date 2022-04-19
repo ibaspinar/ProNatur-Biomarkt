d3000dab1324b6dff6b56042d9584fb8329877b0
ibaspinar <ihsan@baspinar.eu>
2022-04-19 14:56:55 +0200

    Button Save und Clear Methoden angelegt, befüllt, getestet. DataGridView Hintergrund farben angepasst

--- ProNatur-Biomarkt GmbH/InvoiceScreen.Designer.cs
+++ ProNatur-Biomarkt GmbH/InvoiceScreen.Designer.cs
@@ -30,12 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InvoiceScreen));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxInvoiceName = new System.Windows.Forms.TextBox();
+            this.textBoxInvoiceProduct = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxInvoicePrice = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBoxInvoiceCategory = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btnInvoiceSave = new System.Windows.Forms.Button();
             this.btnInvoiceEdit = new System.Windows.Forms.Button();
@@ -57,27 +57,27 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Empfänger";
             // 
-            // textBox1
+            // textBoxInvoiceName
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(165, 10);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(223, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBoxInvoiceName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.textBoxInvoiceName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInvoiceName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInvoiceName.ForeColor = System.Drawing.Color.White;
+            this.textBoxInvoiceName.Location = new System.Drawing.Point(165, 10);
+            this.textBoxInvoiceName.Name = "textBoxInvoiceName";
+            this.textBoxInvoiceName.Size = new System.Drawing.Size(223, 26);
+            this.textBoxInvoiceName.TabIndex = 1;
             // 
-            // textBox2
+            // textBoxInvoiceProduct
             // 
-            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(165, 53);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(223, 26);
-            this.textBox2.TabIndex = 3;
+            this.textBoxInvoiceProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.textBoxInvoiceProduct.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInvoiceProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInvoiceProduct.ForeColor = System.Drawing.Color.White;
+            this.textBoxInvoiceProduct.Location = new System.Drawing.Point(165, 53);
+            this.textBoxInvoiceProduct.Name = "textBoxInvoiceProduct";
+            this.textBoxInvoiceProduct.Size = new System.Drawing.Size(223, 26);
+            this.textBoxInvoiceProduct.TabIndex = 3;
             // 
             // label2
             // 
@@ -91,16 +91,16 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Produkt";
             // 
-            // textBox3
+            // textBoxInvoicePrice
             // 
-            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.ForeColor = System.Drawing.Color.White;
-            this.textBox3.Location = new System.Drawing.Point(165, 137);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(223, 26);
-            this.textBox3.TabIndex = 5;
+            this.textBoxInvoicePrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.textBoxInvoicePrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxInvoicePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInvoicePrice.ForeColor = System.Drawing.Color.White;
+            this.textBoxInvoicePrice.Location = new System.Drawing.Point(165, 137);
+            this.textBoxInvoicePrice.Name = "textBoxInvoicePrice";
+            this.textBoxInvoicePrice.Size = new System.Drawing.Size(223, 26);
+            this.textBoxInvoicePrice.TabIndex = 5;
             // 
             // label3
             // 
@@ -114,17 +114,24 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Gesamtpreis";
             // 
-            // comboBox1
+            // comboBoxInvoiceCategory
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.ForeColor = System.Drawing.Color.White;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(165, 93);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(223, 28);
-            this.comboBox1.TabIndex = 6;
+            this.comboBoxInvoiceCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.comboBoxInvoiceCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxInvoiceCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxInvoiceCategory.ForeColor = System.Drawing.Color.White;
+            this.comboBoxInvoiceCategory.FormattingEnabled = true;
+            this.comboBoxInvoiceCategory.Items.AddRange(new object[] {
+            "Obst",
+            "Gemüse",
+            "Fleisch",
+            "Fisch",
+            "Drogerie",
+            "Getränke"});
+            this.comboBoxInvoiceCategory.Location = new System.Drawing.Point(165, 93);
+            this.comboBoxInvoiceCategory.Name = "comboBoxInvoiceCategory";
+            this.comboBoxInvoiceCategory.Size = new System.Drawing.Size(223, 28);
+            this.comboBoxInvoiceCategory.TabIndex = 6;
             // 
             // label4
             // 
@@ -144,12 +151,13 @@
             this.btnInvoiceSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInvoiceSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInvoiceSave.ForeColor = System.Drawing.Color.White;
-            this.btnInvoiceSave.Location = new System.Drawing.Point(575, 4);
+            this.btnInvoiceSave.Location = new System.Drawing.Point(568, 9);
             this.btnInvoiceSave.Name = "btnInvoiceSave";
             this.btnInvoiceSave.Size = new System.Drawing.Size(180, 37);
             this.btnInvoiceSave.TabIndex = 8;
             this.btnInvoiceSave.Text = "Speichern";
             this.btnInvoiceSave.UseVisualStyleBackColor = true;
+            this.btnInvoiceSave.Click += new System.EventHandler(this.btnInvoiceSave_Click);
             // 
             // btnInvoiceEdit
             // 
@@ -157,12 +165,13 @@
             this.btnInvoiceEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInvoiceEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInvoiceEdit.ForeColor = System.Drawing.Color.White;
-            this.btnInvoiceEdit.Location = new System.Drawing.Point(761, 4);
+            this.btnInvoiceEdit.Location = new System.Drawing.Point(754, 9);
             this.btnInvoiceEdit.Name = "btnInvoiceEdit";
             this.btnInvoiceEdit.Size = new System.Drawing.Size(180, 37);
             this.btnInvoiceEdit.TabIndex = 9;
             this.btnInvoiceEdit.Text = "Bearbeiten";
             this.btnInvoiceEdit.UseVisualStyleBackColor = true;
+            this.btnInvoiceEdit.Click += new System.EventHandler(this.btnInvoiceEdit_Click);
             // 
             // btnInvoiceClear
             // 
@@ -170,12 +179,13 @@
             this.btnInvoiceClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInvoiceClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInvoiceClear.ForeColor = System.Drawing.Color.White;
-            this.btnInvoiceClear.Location = new System.Drawing.Point(947, 4);
+            this.btnInvoiceClear.Location = new System.Drawing.Point(940, 9);
             this.btnInvoiceClear.Name = "btnInvoiceClear";
             this.btnInvoiceClear.Size = new System.Drawing.Size(180, 37);
             this.btnInvoiceClear.TabIndex = 10;
             this.btnInvoiceClear.Text = "Felder Leeren";
             this.btnInvoiceClear.UseVisualStyleBackColor = true;
+            this.btnInvoiceClear.Click += new System.EventHandler(this.btnInvoiceClear_Click);
             // 
             // btnInvoiceDelete
             // 
@@ -183,21 +193,30 @@
             this.btnInvoiceDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInvoiceDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInvoiceDelete.ForeColor = System.Drawing.Color.White;
-            this.btnInvoiceDelete.Location = new System.Drawing.Point(1133, 4);
+            this.btnInvoiceDelete.Location = new System.Drawing.Point(1126, 9);
             this.btnInvoiceDelete.Name = "btnInvoiceDelete";
             this.btnInvoiceDelete.Size = new System.Drawing.Size(180, 37);
             this.btnInvoiceDelete.TabIndex = 11;
             this.btnInvoiceDelete.Text = "Löschen";
             this.btnInvoiceDelete.UseVisualStyleBackColor = true;
+            this.btnInvoiceDelete.Click += new System.EventHandler(this.btnInvoiceDelete_Click);
             // 
             // invoiceDGV
             // 
+            this.invoiceDGV.AllowUserToAddRows = false;
+            this.invoiceDGV.AllowUserToDeleteRows = false;
+            this.invoiceDGV.AllowUserToResizeColumns = false;
+            this.invoiceDGV.AllowUserToResizeRows = false;
+            this.invoiceDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.invoiceDGV.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.invoiceDGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.invoiceDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.invoiceDGV.Location = new System.Drawing.Point(12, 179);
             this.invoiceDGV.Name = "invoiceDGV";
+            this.invoiceDGV.RowHeadersVisible = false;
             this.invoiceDGV.RowHeadersWidth = 51;
+            this.invoiceDGV.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.invoiceDGV.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.White;
             this.invoiceDGV.RowTemplate.Height = 24;
             this.invoiceDGV.Size = new System.Drawing.Size(1290, 612);
             this.invoiceDGV.TabIndex = 12;
@@ -215,12 +234,12 @@
             this.Controls.Add(this.btnInvoiceEdit);
             this.Controls.Add(this.btnInvoiceSave);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBoxInvoiceCategory);
+            this.Controls.Add(this.textBoxInvoicePrice);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxInvoiceProduct);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxInvoiceName);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -236,12 +255,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxInvoiceName;
+        private System.Windows.Forms.TextBox textBoxInvoiceProduct;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxInvoicePrice;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxInvoiceCategory;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnInvoiceSave;
         private System.Windows.Forms.Button btnInvoiceEdit;
--- ProNatur-Biomarkt GmbH/InvoiceScreen.cs
+++ ProNatur-Biomarkt GmbH/InvoiceScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,110 @@ namespace ProNatur_Biomarkt_GmbH
 {
     public partial class InvoiceScreen : Form
     {
+        private SqlConnection databaseConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Anwender\Documents\ProNatur Biomarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
+
+        private int lastSelectedProductKey;
+
         public InvoiceScreen()
         {
             InitializeComponent();
+            ShowInvoice();
+        }
+
+        private void btnInvoiceSave_Click(object sender, EventArgs e)
+        {
+            if (textBoxInvoiceName.Text == ""
+                || textBoxInvoiceProduct.Text == ""
+                || textBoxInvoicePrice.Text == ""
+                || comboBoxInvoiceCategory.Text == "")
+            {
+                MessageBox.Show("Bitte alle Felder füllen!");
+                return;
+            }
+
+            // save product name in database
+            string invoiceName = textBoxInvoiceName.Text;
+            string invoiceProduct = textBoxInvoiceProduct.Text;
+            string invoiceCategory = comboBoxInvoiceCategory.Text;
+            string invoicePrice = textBoxInvoicePrice.Text;
+            
+            string query = string.Format("insert into Invoice values('{0}', '{1}', '{2}', '{3}')", invoiceName, invoiceProduct, invoiceCategory, invoicePrice);
+            ExecuteQuery(query);
+
+            ClearFelder();
+            ShowInvoice();
+        }
+
+        private void btnInvoiceEdit_Click(object sender, EventArgs e)
+        {
+            if (lastSelectedProductKey == 0)
+            {
+                MessageBox.Show("Bitte wähle erst ein Produkt aus.");
+                return;
+            }
+
+            string invoiceName = textBoxInvoiceName.Text;
+            string invoiceProduct = textBoxInvoiceProduct.Text;
+            string invoiceCategory = comboBoxInvoiceCategory.Text;
+            string invoicePrice = textBoxInvoicePrice.Text;
+
+            string query = string.Format("update Invoice set Empfänger='{0}', Produkt='{1}', Kategorie='{2}', Gesamtpreis='{3}' where Id={4}"
+                , invoiceName, invoiceProduct, invoiceCategory, invoicePrice, lastSelectedProductKey);
+
+            ExecuteQuery(query);
+
+            ShowInvoice();
+        }
+
+        private void btnInvoiceClear_Click(object sender, EventArgs e)
+        {
+            ClearFelder();
+        }
+
+        private void btnInvoiceDelete_Click(object sender, EventArgs e)
+        {
+
+            ClearFelder();
+            ShowInvoice();
+        }
+
+        // Zeige Rechnungen bei Start
+        private void ShowInvoice()
+        {
+            // Start
+            databaseConnection.Open();
+
+            string query = "Select * from dbo.Invoice";
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, databaseConnection);
+
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+
+            invoiceDGV.DataSource = dataSet.Tables[0];
+
+            invoiceDGV.Columns[0].Visible = false;
+
+            databaseConnection.Close();
+        }
+
+        //Datenbank connecten und mit query übergeben
+        private void ExecuteQuery(string query)
+        {
+            databaseConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(query, databaseConnection);
+            sqlCommand.ExecuteNonQuery();
+            databaseConnection.Close();
+        }
+
+        // Lösche Inhalt
+        public void ClearFelder()
+        {
+            textBoxInvoiceName.Text = "";
+            textBoxInvoiceProduct.Text = "";
+            comboBoxInvoiceCategory.Text = "";
+            textBoxInvoicePrice.Text = "";
+            comboBoxInvoiceCategory.SelectedItem = null;
         }
     }
 }
